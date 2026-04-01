687c6b3def2068f38f33e8ab2ac1829a17f35897
Ayushchaurasia24 <chaurasiakd9425@gmail.com>
2026-04-01 17:28:36 +0530

    Add login API with authentication using bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,3 +29,30 @@ exports.signup = async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. Check if user exists
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // 2. Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "User not authorized" });
+    }
+
+    // 3. Success
+    return res.status(200).json({ message: "User login successful" });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
